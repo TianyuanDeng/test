5a6556afe53e220d90f45f432bc4ac321e65089e
Tianyuan Deng <wergelesn@missouri.edu>
2018-10-19 15:58:08 -0500

    Add color struct  and set background color

---
+++ Colors/Colors/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  Colors
+//
+//  Created by Deng tianyuan on 10/19/18.
+//  Copyright © 2018 Deng tianyuan. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    let name:String
+    let uiColor:UIColor
+}
