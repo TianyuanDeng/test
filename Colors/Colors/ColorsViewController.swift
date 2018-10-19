a40a40e4bd7311df0835990787c0cd0f326ad934
Tianyuan Deng <wergelesn@missouri.edu>
2018-10-19 16:45:34 -0500

    Update the detail view of color

--- Colors/Colors/ColorsViewController.swift
+++ Colors/Colors/ColorsViewController.swift
@@ -19,6 +19,9 @@ class ColorsViewController: UIViewController, UITableViewDelegate, UITableViewDa
                   Color(name: "blue", uiColor: UIColor.blue),
                   Color(name: "purple", uiColor: UIColor.purple),
                   Color(name: "brown", uiColor: UIColor.brown)]
+    
+    @IBOutlet weak var colorsTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad();
 
@@ -48,14 +51,14 @@ class ColorsViewController: UIViewController, UITableViewDelegate, UITableViewDa
         
         return cell
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.cellForRow(at: indexPath)?.isSelected = false
     }
-    */
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? ColorDetailViewController, let row = colorsTableView.indexPathForSelectedRow?.row {
+            destination.color = colors[row]
+        }
+    }
 }
